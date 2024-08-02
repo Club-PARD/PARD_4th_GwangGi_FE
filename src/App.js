618e7082ef18c547a4888c3dddc66f7b3f8e749f
pangil5634 <oksk6681@naver.com>
2024-08-02 14:18:49 +0900

    add : Style Theme file and file setting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import {BrowserRouter} from 'react-router-dom';
 import './App.css';
+import Router from './Layout/Router';
+import Map from './Test/Map';
+import { theme } from './Style/theme';
+import { ThemeProvider } from 'styled-components';
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <Router/>
+            </ThemeProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/Style/theme.js
@@ -0,0 +1,13 @@
+// theme.js
+export const theme = {
+    colors: {
+        primary: '#0070f3',
+        secondary: '#1A202C',
+        accent: '#2D3748',
+        background: '#F7FAFC',
+        text: '#333'
+    },
+    fonts: {
+        main: 'Arial, sans-serif'
+    }
+};
