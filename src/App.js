6527969e843d1585da96a2f77002df923e1146a4
codms0201 <chanyeol0615@naver.com>
2024-08-02 17:39:13 +0900

    풀용 확인

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Router from './Layout/Router';
 import {theme} from './Style/theme';
 import {ThemeProvider} from 'styled-components';
+//채채 왔다감
 
 function App() {
     return (
