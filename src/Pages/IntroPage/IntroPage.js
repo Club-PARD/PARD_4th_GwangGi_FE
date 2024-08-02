6ba88585b5d20d965c3ed1137ba6b47cab3cb542
pangil5634 <oksk6681@naver.com>
2024-08-02 23:04:38 +0900

    edit : API Test buttong

--- src/Pages/IntroPage/IntroPage.js
+++ src/Pages/IntroPage/IntroPage.js
@@ -1,10 +1,15 @@
+import { testAPI } from "../../API/TestAPI";
 import { BaseContainer } from "../../Layout/Container";
 
 function IntroPage() {
-    
+    const handleTestAPI = () => {
+        const result = testAPI();
+        console.log(result);
+    }
     return (
         <BaseContainer>
             IntroPage
+            <button onClick = {handleTestAPI}>테스트</button>
         </BaseContainer>
     )
 }
