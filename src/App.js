ecc06ba00006a491e6ca8cba00e88b5ae82e7041
codms0201 <chanyeol0615@naver.com>
2024-08-02 17:30:19 +0900

    Delete test/map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import {BrowserRouter} from 'react-router-dom';
 import './App.css';
 import Router from './Layout/Router';
-import Map from './Test/Map';
 import { theme } from './Style/theme';
 import { ThemeProvider } from 'styled-components';
 
