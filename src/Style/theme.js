8909c2b7980a0ca39d0ea09eb67c9f2ba2e2f073
pangil5634 <oksk6681@naver.com>
2024-08-04 01:22:23 +0900

    edit : che, pang colors

--- src/Style/theme.js
+++ src/Style/theme.js
@@ -1,11 +1,17 @@
 // theme.js
 export const theme = {
     colors: {
-        primary: '#0070f3',
+        primary: '#fff',
         secondary: '#1A202C',
         accent: '#2D3748',
         background: '#F7FAFC',
         text: '#333'
+    },
+    che_colors: {
+        main_color: '#F7FAFC',
+    },
+    pang_colors: {
+        
     },
     fonts: {
         main: 'Arial, sans-serif'
