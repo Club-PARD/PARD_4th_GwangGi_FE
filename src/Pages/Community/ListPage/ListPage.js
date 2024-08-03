847f2f5868e3bf2156410ad76fdeb512fbe70445
pangil5634 <oksk6681@naver.com>
2024-08-04 00:38:51 +0900

    edit : ListPage MockData, FlexContainer

--- src/Pages/Community/ListPage/ListPage.js
+++ src/Pages/Community/ListPage/ListPage.js
@@ -1,10 +1,31 @@
+import styled from "styled-components";
+import { FlexContainer } from "../../../Layout/Container";
+import { ListDataMock } from "./ListDataMock";
+
 function ListPage() {
-    
+
     return (
-        <div>
-            ListPage
-        </div>
+        <FlexContainer>
+            {
+                ListDataMock.map((ChallengeInfo, index) => (
+                    <ListDataContainer key={index}>
+                        {ChallengeInfo.challenge_id}
+                    </ListDataContainer>
+                ))
+            }
+        </FlexContainer>
     )
 }
 
+const ListDataContainer = styled.div`
+    width: 100%;
+    height : 200px;
+    background-color: white;
+
+    margin-bottom: 20px;
+
+    border : 2px solid skyblue;
+    border-radius: 10px;
+    box-sizing: border-box;
+`;
 export default ListPage;
