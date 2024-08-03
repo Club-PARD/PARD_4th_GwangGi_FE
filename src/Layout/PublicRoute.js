06a6f652c9b8c51dd2632cdcd80307c3baef8f9f
pangil5634 <oksk6681@naver.com>
2024-08-04 01:31:36 +0900

    edit : 이미 로그인한 상태 추가

--- src/Layout/PublicRoute.js
+++ src/Layout/PublicRoute.js
@@ -3,12 +3,11 @@ import {Navigate} from "react-router-dom";
 
 function PublicRoute({
     element,
-    restricted,
     ...rest
 }) {
     const email = sessionStorage.getItem("user_email");
 
-    if (email && restricted) {
+    if (email) {
         alert("이미 로그인한 상태입니다.");
         return <Navigate to="/home"/>;
     }
