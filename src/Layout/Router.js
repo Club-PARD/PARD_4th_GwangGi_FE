8a236805fcce918670f2c1994865883374a4cc18
pangil5634 <oksk6681@naver.com>
2024-08-04 05:51:17 +0900

    edit : Public -> Private

--- src/Layout/Router.js
+++ src/Layout/Router.js
@@ -22,7 +22,7 @@ function Router() {
             />
             <Route
                 path="/register"
-                element={<PublicRoute element={<RegisterPage />} />}
+                element={<PrivateRoute element={<RegisterPage />} />}
             />
             <Route element={<Layout />}>
                 <Route
