d0f7f49d868ed963bdd13815223fe562352c0caa
pangil5634 <oksk6681@naver.com>
2024-08-04 05:51:34 +0900

    feat : get User Info by API

---
+++ src/API/UserAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const getUserInfo = async () => {
+    try {
+        const response = await axios.get(
+            `${process.env.REACT_APP_URL}api/user`,
+            {
+                withCredentials: true,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+        console.log("handlePostRegsiter success");
+        return response.data.response_object;
+    } catch (error) {
+        console.log("handlePostRegsiter fail", error);
+    }
+}
