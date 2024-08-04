87336d44f88dc9c4a2d17e1b943d29e1a779fea7
pangil5634 <oksk6681@naver.com>
2024-08-05 01:34:59 +0900

    edit : Link to page

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,10 +1,12 @@
-import { handleTest } from "../../API/LoginAPI";
+import { Link } from "react-router-dom";
+import { getUserInfo } from "../../API/UserAPI";
 
 function HomePage() {
     
     return (
         <div>
-            <button onClick={handleTest}>테스트</button>
+            <button onClick={getUserInfo}>테스트</button>
+            <Link to = "/mypage">마이 페이지</Link>
         </div>
     )
 }
