bef1513c2ed8e10e3744f5afaa771ff1b4388cf2
pangil5634 <oksk6681@naver.com>
2024-08-06 17:20:09 +0900

    edit : MockupData add

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -2,36 +2,11 @@ import styled from "styled-components";
 import { FlexContainer } from "../../Layout/Container"
 import InfoBoxComponent from "./Components/InfoBoxComponent";
 import BestChallengComponent from "./Components/BestChallengComponent";
+import { useState } from "react";
+import MockUpBestChallengeInfo from "./Components/MockupData";
+
 function HomePage() {
-    const BestChallengeInfo = [
-        {
-            bestChallengeId: 1,
-            bestChallengeTitle: "함께 하실 분?",
-            bestChallengeAge: 50,
-            bestChallengeGender: 0,
-            bestChallengeOrg: "대구",
-            bestChallengeDate: "2024-08-01",
-            bestChallengePeopleCount : 5
-        },
-        {
-            bestChallengeId: 2,
-            bestChallengeTitle: "여고생 모여라",
-            bestChallengeAge: 10,
-            bestChallengeGender: 1,
-            bestChallengeOrg: "포항",
-            bestChallengeDate : "2024-08-12",
-            bestChallengePeopleCount : 8
-        },
-        {
-            bestChallengeId: 3,
-            bestChallengeTitle: "신혼 부부 환영",
-            bestChallengeAge: 30,
-            bestChallengeGender: 2,
-            bestChallengeOrg: "서울 동작구 헌혈의 집",
-            bestChallengeDate : "2024-08-06",
-            bestChallengePeopleCount : 10
-        }
-    ];
+    const [BestChallengeInfo, setBestChallengeInfo] =  useState(MockUpBestChallengeInfo || []);
     return (
         <HomePageContainer>
             <InfoBoxComponent/>
